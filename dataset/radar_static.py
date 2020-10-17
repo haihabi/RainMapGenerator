65159d47d944d974924549182d3e767a27b03c37
haihabi1986@gmail.com <K18118Wre>
2020-10-17 13:09:46 +0300

    update dataset to support pickle

--- dataset/radar_static.py
+++ dataset/radar_static.py
@@ -1,51 +1,28 @@
-import numpy as np
-import random
-from dataset.data_common import read_radar_files, load_radar_image, preprocess_radar_data, sample_slice
+from dataset.data_common import radar2rain
 from torch.utils.data import Dataset
+import pickle
 
 
-class RadarStaticDataSet(Dataset):
-    def __init__(self, base_folder: str, h: int, w: int, n_samples4epoch=50000, n_total_samples=100000, min_points=10,
-                 download=False, pickle=False):
-        self.base_folder = base_folder
-        self.data_dict = dict()
-        self.data_dict_keys = list(self.data_dict.keys())
-        self.file_dict = read_radar_files(base_folder)
-        self.file_list = list(self.file_dict.values())
-        self.n_total_samples = n_total_samples
-        self.n_samples4epoch = n_samples4epoch
-        self.h = h
-        self.w = w
-        self.n_samples = 0
-        self.min_points = min_points
-        if download:
-            pass
-        print("The total number of files:" + str(len(self.file_list)))
+class RadarDataSet(Dataset):
+    def __init__(self, pickle_path: str):
+        self.pickle_path = pickle_path
+        self.pickle_data = pickle.load(open(pickle_path, 'rb'))
+        self.data_preprocess = dict()
+        self.n_samples = len(self.pickle_data)
 
     def __len__(self):
-        return self.n_samples4epoch
+        return self.n_samples
 
     def __getitem__(self, index):
-        return self.get_sample()
+        return self.get_sample(index)
 
-    def get_sample(self):
-        index = int(np.random.randint(0, len(self.file_list), 1).astype('int'))
-        if self.data_dict.get(index) is None and not self.n_samples >= self.n_total_samples:
-            r = []
-            while len(r) <= 1:
-                data_full = load_radar_image(self.file_list[index]).astype('float32')
-                data_full, r, c = preprocess_radar_data(data_full, self.h, self.w, self.min_points)
-                if len(r) <= 1:
-                    index = int(np.random.randint(0, len(self.file_list), 1).astype('int'))
-
-            self.n_samples += len(r)
-            self.data_dict.update({index: (data_full, r, c)})
-            self.data_dict_keys = list(self.data_dict.keys())
-        else:
-            index = random.choice(self.data_dict_keys)
-            data_full, r, c = self.data_dict.get(index)
-        d = sample_slice(data_full, r, c, self.h, self.w)
-        return d
+    def get_sample(self, index):
+        data_image = self.data_preprocess.get(index)
+        if data_image is None:
+            sample = self.pickle_data[index]
+            data_image = radar2rain(sample)
+            self.data_preprocess.update({index: data_image})
+        return data_image
 
     def number_of_files_load(self):
         return len(self.data_dict)
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+from dataset.radar_static import RadarDataSet
 from google.colab import drive
 
 drive.mount('/content/gdrive/')
-import os
-import pickle
 
 PROJECT = 'RainMapGenerator'
 
 if __name__ == '__main__':
     print(f"Starting Run of {PROJECT}")
     data_file = '/content/gdrive/My Drive/Runners/Data/rain_data.pickle'
-    data = pickle.load(open(data_file, 'rb'))
-    print(len(data))
+    rds = RadarDataSet(data_file)
+    print(len(rds))
