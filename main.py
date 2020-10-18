0d5d5980cd7062cbfcb30db6e05d0ecb252fe26d
haihabi1986@gmail.com <K18118Wre>
2020-10-18 16:44:44 +0300

    adding argument parser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+import argparse
 from torchvision.transforms import RandomHorizontalFlip, \
     RandomVerticalFlip
 from importlib import util
@@ -27,29 +28,54 @@ PROJECT = 'RainMapGenerator'
 
 data_file = '/content/gdrive/My Drive/Runners/Data/rain_data.pickle'
 data_file_val = '/content/gdrive/My Drive/Runners/Data/rain_data_val.pickle'
-seed = 0
+
 batch_size = 32
 h = 32
 w = 32
-z_size = 128
+# z_size = 128
 dim = 128
 lr_g = 1e-4
 lr_d = 2e-4
 betas = (0.5, 0.999)
 wd = 1e-4
-epoch = 20
-sn_enable = True
+
+
+def arg_parsing():
+    parser = argparse.ArgumentParser(description='Rain Map Generative Training')
+    parser.add_argument('seed', type=int, default=0)
+    parser.add_argument('n_epoch', type=int, default=20)
+
+    parser.add_argument('training_data_pickle', type=str,
+                        default='/content/gdrive/My Drive/Runners/Data/rain_data.pickle')
+    parser.add_argument('training_data_pickle', type=str,
+                        default='/content/gdrive/My Drive/Runners/Data/rain_data.pickle')
+    ################################
+    # Optimizer
+    ################################
+    ################################
+    # GAN
+    ################################
+    parser.add_argument('z_size', type=int, default=128)
+    parser.add_argument('sn_enable', action='store_true')
+    ################################
+    # Network Config
+    ################################
+    args = parser.parse_args()
+
+    return args
+
+
 if __name__ == '__main__':
     if google_flag:
         print("Mounting Drive Folder...")
         drive.mount('/content/gdrive/')
 
-    args = {}
+    args = arg_parsing()
     print(f"Starting Run of {PROJECT}")
     if found_wandb:
         wandb.init(project=PROJECT)
         wandb.config.update(args)  # adds all of the arguments as config variables
-    torch.manual_seed(seed)
+    torch.manual_seed(args.seed)
     working_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Current Working Device is set to:" + str(working_device))
 
@@ -70,17 +96,17 @@ if __name__ == '__main__':
                                                     batch_size=batch_size,
                                                     shuffle=False)
     fid = FrechetInceptionDistance(batch_size, validation_loader, working_device)
-    net_g, net_d = get_network(z_size, dim, h, w, working_device)
+    net_g, net_d = get_network(args.z_size, dim, h, w, working_device)
 
     optimizer_d = optim.Adam(net_d.parameters(), lr=lr_d, betas=betas, weight_decay=wd)
     optimizer_g = optim.Adam(net_g.parameters(), lr=lr_g, betas=betas, weight_decay=wd)
 
-    gan_cfg = gan.GANConfig(gan.GANType.RaSGAN, batch_size=batch_size, z_size=z_size,
-                            input_working_device=working_device, sn_enable=sn_enable)
+    gan_cfg = gan.GANConfig(gan.GANType.RaSGAN, batch_size=batch_size, z_size=args.z_size,
+                            input_working_device=working_device, sn_enable=args.sn_enable)
     gan_trainer = gan.GANTraining(gan_cfg, net_d, net_g, optimizer_d, optimizer_g)
 
     ra = ResultsAveraging()
-    for i in range(epoch):
+    for i in range(args.epoch):
         for data in tqdm(train_loader):
             data = data.to(working_device)
             data = data.float()
