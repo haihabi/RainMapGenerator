3475ba6f8428eca34914c12744c13b3015e5e33f
haihabi1986@gmail.com <haihabi1986@gmail.com>
2021-04-30 12:41:54 +0300

    bug fix in working device

--- networks/factory.py
+++ networks/factory.py
@@ -10,5 +10,5 @@ class NetworkName(Enum):
 def get_network(z_size, dim, h, w, vae_enable, input_working_device):
     net_g = Generator(dim, h, w, z_size=z_size).to(input_working_device)
     net_d = Discriminator(dim, h, w).to(input_working_device)
-    net_e = Encoder(dim, z_size, h, w) if vae_enable else None
+    net_e = Encoder(dim, z_size, h, w).to(input_working_device) if vae_enable else None
     return net_g, net_d, net_e
